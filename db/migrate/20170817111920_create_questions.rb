3fbb2068a870fc7842a75ee2657015188ea63ef2
Blake Meyer & Carter McGee & younghoonkwon <pair+blakeynwa.carterm912.younghoonkwon@devbootcamp.com>
2017-08-17 11:30:24 -0700

    create models and migrations

---
+++ db/migrate/20170817111920_create_questions.rb
@@ -0,0 +1,11 @@
+class CreateQuestions < ActiveRecord::Migration[5.1]
+  def change
+    create_table :questions do |t|
+      t.integer :user_id
+      t.string :title, null: false
+      t.string :body, null:false
+
+      t.timestamp
+    end
+  end
+end
