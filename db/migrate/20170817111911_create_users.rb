5c95574465d9300c434eb6f76e0d67a9f5ed146d
Blake Meyer & Carter McGee & younghoonkwon <pair+blakeynwa.carterm912.younghoonkwon@devbootcamp.com>
2017-08-17 12:18:52 -0700

    Made changes to users model and user post RESTful route
    Added first_name and last_name in users model as required field for creating a user acount
    Added content to the post route for users to assist with creating a user account

--- db/migrate/20170817111911_create_users.rb
+++ db/migrate/20170817111911_create_users.rb
@@ -2,6 +2,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :user_name, null: false, unique: true
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.string :email, null:false, unique: true
       t.string :hashed_password, null: false
 
