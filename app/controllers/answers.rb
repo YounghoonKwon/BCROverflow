33c69ecdb1cd8d5cd79badea4aece8f89d656d5a
Blake Meyer & Carter McGee & younghoonkwon <pair+blakeynwa.carterm912.younghoonkwon@devbootcamp.com>
2017-08-18 12:25:29 -0700

    Working on answers

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -1,8 +1,18 @@
+get '/questions/:id/answers/new' do
+  @question = Question.find(params[:id])
+  erb :'answers/new'
+end
+
 get '/answers/:id' do
   erb :'answers/index'
 end
 
-post '/answers' do
-  redirect '/answers/:id'
+post '/questions/:id/answers' do
+  question = Question.find(params[:id])
+  answer_list = params[:answer]
+  @answer = Answer.create(text: answer_list[:text])
+  puts @answer.inspect
+  puts @answer[:question_id]
+  redirect "/questions/#{@question.id}"
 end
 
