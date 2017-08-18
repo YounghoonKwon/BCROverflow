f67e8520a570ab5f3d1f083bfee24e6ae8da53af
Blake Meyer & Carter McGee & younghoonkwon <pair+blakeynwa.carterm912.younghoonkwon@devbootcamp.com>
2017-08-18 15:45:15 -0700

    Added answering function to questions

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -10,7 +10,7 @@ end
 post '/questions/:id/answers' do
   @question = Question.find(params[:id])
   answer_list = params[:answer]
-  @answer = Answer.create(text: answer_list[:text], question_id: params[:id])
+  @answer = Answer.create(text: answer_list[:text], question_id: params[:id], user_id: session[:user_id])
   redirect "/questions/#{@question.id}"
 end
 
