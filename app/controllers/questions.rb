6c6a366b6babe20a7d72eabf6f945a776ee6e43a
Blake Meyer & Carter McGee & younghoonkwon <pair+blakeynwa.carterm912.younghoonkwon@devbootcamp.com>
2017-08-18 11:09:07 -0700

    Added edit and delete function to the questions

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -9,7 +9,7 @@ end
 
 post '/questions' do
   question_list = params[:question]
-  @question = Question.create(title: question_list[:title], body: question_list[:body])
+  @question = Question.create(title: question_list[:title], body: question_list[:body], user_id: session[:user_id])
   redirect "/questions/#{@question.id}"
 end
 
@@ -19,11 +19,23 @@ get '/questions/:id' do
 end
 
 delete '/questions/:id' do
-  puts params
   @question = Question.find(params[:id])
   @question.destroy!
   redirect '/questions'
 end
+
+get '/questions/:id/edit' do
+  @question = Question.find(params[:id])
+  erb :'questions/edit'
+end
+
+put '/questions/:id' do
+  @question = Question.find(params[:id])
+  @question.update(params[:question])
+  redirect "questions/#{@question.id}"
+end
+
+
 
 
 
