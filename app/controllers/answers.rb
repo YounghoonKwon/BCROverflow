4e9e4dbec357c469e9b9e686bbcf6e3eae4d23b8
Blake Meyer & Carter McGee & younghoonkwon <pair+blakeynwa.carterm912.younghoonkwon@devbootcamp.com>
2017-08-19 15:10:20 -0700

    Added some jquery and ajax, about to make some more changes but don't want to destroy everything

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -11,6 +11,15 @@ post '/questions/:id/answers' do
   @question = Question.find(params[:id])
   answer_list = params[:answer]
   @answer = Answer.create(text: answer_list[:text], question_id: params[:id], user_id: session[:user_id])
-  redirect "/questions/#{@question.id}"
+  if request.xhr?
+    erb :'answers/new', layout: false, answer: @answer
+  else
+    redirect "/questions/#{@question.id}"
+  end
 end
 
+  # if request.accept? 'application/json'
+    #   @answer.to_json
+    # else
+    #   @answer.to_s
+    # end
--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
-  $(".answer_buttons").find("a").on('click', function(e){
+  // $(".answer_buttons").find("a").on('click', function(e){
+  //   e.preventDefault()
+  //   $("div[class='answer']").slideToggle()
+  // })
+  $(".submit_answer").on('submit', function(e) {
     e.preventDefault()
-    $("div[class='answer']").slideToggle();
+    var $this = $(this)
+    var url =$this.closest("form").attr('action')
+    $.ajax({
+      url: url,
+      method: 'POST',
+      data: $this.serialize()
+    }).done(function(response){
+      $('.answer_list').append(response)
+
+    })
+
   })
 });
