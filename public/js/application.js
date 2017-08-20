e9740f11206099eaebaf605bdd46007d1fd293e1
Blake Meyer & Carter McGee & younghoonkwon <pair+blakeynwa.carterm912.younghoonkwon@devbootcamp.com>
2017-08-19 18:41:27 -0700

    Styling changes, upvote/downvote works

--- public/js/application.js
+++ public/js/application.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
-  $(".answer_buttons").find("a").on('click', function(e){
+  $(".answer_buttons").find("a").on('click', answer_buttons)
+  $(".submit_answer").on('submit', submit_answer)
+  $(".question_upvote").on('submit', question_upvote)
+  $(".question_downvote").on('submit', question_downvote)
+  $(".answer_upvote").on('submit', answer_upvote)
+  $(".answer_downvote").on('submit', answer_downvote)
+});
+
+
+var answer_buttons = function(e){
     e.preventDefault()
     $("div[class='answer']").slideToggle()
-  })
-  $(".submit_answer").on('submit', function(e) {
+  }
+
+var submit_answer = function(e) {
     e.preventDefault()
     var $this = $(this)
     var url =$this.closest("form").attr('action')
@@ -14,8 +24,9 @@ $(document).ready(function() {
     }).done(function(response){
       $('.answer_list').append(response)
     })
-  })
-  $(".question_upvote").on('submit', function(e){
+  }
+
+var question_upvote = function(e){
     e.preventDefault()
     var $this = $(this)
     var url = $this.closest("form").attr('action')
@@ -23,10 +34,11 @@ $(document).ready(function() {
       url: url,
       method: 'POST'
     }).done(function(response){
-      $(".question_vote_count").text(response)
+      $(".question_vote_count").text("question score: " + response)
     })
-  })
-  $(".question_downvote").on('submit', function(e){
+  }
+
+var question_downvote = function(e){
     e.preventDefault()
     var $this = $(this)
     var url = $this.closest("form").attr('action')
@@ -34,10 +46,11 @@ $(document).ready(function() {
       url: url,
       method: 'POST'
     }).done(function(response){
-      $(".question_vote_count").text(response)
+      $(".question_vote_count").text("question score: " + response)
     })
-  })
-  $(".answer_upvote").on('submit', function(e){
+  }
+
+var answer_upvote = function(e){
     e.preventDefault()
     var $this = $(this)
     var url = $this.closest("form").attr('action')
@@ -45,10 +58,11 @@ $(document).ready(function() {
       url: url,
       method: 'POST'
     }).done(function(response){
-      $($this).siblings(".answer_vote_count").text(response)
+      $($this).siblings(".answer_vote_count").text("answer score: " + response)
     })
-  })
-  $(".answer_downvote").on('submit', function(e){
+  }
+
+var answer_downvote = function(e){
     e.preventDefault()
     var $this = $(this)
     var url = $this.closest("form").attr('action')
@@ -56,7 +70,9 @@ $(document).ready(function() {
       url: url,
       method: 'POST'
     }).done(function(response){
-      $($this).siblings(".answer_vote_count").text(response)
+      $($this).siblings(".answer_vote_count").text("answer score: " + response)
     })
-  })
-});
+  }
+
+
+
